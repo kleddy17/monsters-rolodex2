7aa9aa1a88e5099c01d62edeb5065f4ce122c9d3
kleddy17 <kleddy17@gmail.com>
2021-09-06 09:34:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello Kristi'
+    };
+  }
+  render() {
+    return (   
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+     <button onClick= {()=> this.setState({string: 'Hello Lucy'})}>Change Text</button>
+        </header>
+  </div>)
+  }
+}
+export default App;
