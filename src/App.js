eaff8d67e8d72880120e4411a03fc10632224d5b
kleddy17 <kleddy17@gmail.com>
2021-09-06 11:45:49 -0400

    setting up state

--- src/App.js
+++ src/App.js
@@ -7,17 +7,29 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Kristi'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: 'ascl'
+        },
+        {
+          name: 'Dracula',
+          id: 'asr2'
+
+        },
+        {
+          name: 'Zombie',
+          id: 'aslw'
+        },
+      ]
     };
   }
   render() {
     return (   
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-     <button onClick= {()=> this.setState({string: 'Hello Lucy'})}>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map (monster=><h1 key={monster.id}>{monster.name}</h1>)
+        }
   </div>)
   }
 }
